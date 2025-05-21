07f9b30426e2a8c7449c6345ea23353f19f847d2
BonnySveta <bonnysveta@gmail.com>
2025-05-22 00:33:03 +1000

    Обновлен раздел "Начинающим"

--- a11y-handbook/src/pages/GettingStarted/GettingStarted.tsx
+++ a11y-handbook/src/pages/GettingStarted/GettingStarted.tsx
@@ -17,13 +17,38 @@ const Text = styled.p`
   margin-bottom: 1rem;
 `;
 
+const List = styled.ul`
+  color: var(--text-secondary);
+  margin-bottom: 1.5rem;
+  padding-left: 1.2em;
+`;
+
+const ListItem = styled.li`
+  margin-bottom: 0.7em;
+`;
+
 export function GettingStarted() {
   return (
     <PageLayout title="Начинающим">
       <Section>
         <Text>
-          Этот раздел находится в разработке. <br />
-          Пожалуйста предложите ваши варианты материалов, которые могут быть полезны начинающим на странице <Link to="/feedback">Обратная связь</Link>.
+          Добро пожаловать! Если вы только начинаете знакомство с цифровой доступностью, начните с этих базовых материалов. Они помогут разобраться в основных понятиях, терминах и принципах доступности для всех пользователей.
+        </Text>
+        <SectionTitle>Базовые материалы по цифровой доступности</SectionTitle>
+        <List>
+          <ListItem>
+            <a href="https://trends.rbc.ru/trends/social/63aa999c9a79471fb4e0c276" target="_blank" rel="noopener noreferrer">
+            Полный доступ: что такое цифровая доступность и почему она нужна всем
+            </a> — краткое введение и зачем это нужно.
+          </ListItem>
+          <ListItem>
+            <a href="https://guides.kontur.ru/principles/accessibility/accessibility/" target="_blank" rel="noopener noreferrer">
+              Принципы доступности
+            </a> — виды ограничений и основные принципы.
+          </ListItem>
+        </List>
+        <Text>
+          Если у вас есть предложения по материалам для начинающих — напишите нам через страницу <Link to="/feedback">Обратная связь</Link>!
         </Text>
       </Section>
     </PageLayout>
